7c448dcefb2b384a440f0fddf78e5c0d3874ce12
Blake Barfield <blakebarfield@me.com>
2020-03-08 09:30:25 -0400

    base UI for home screen and route plotting

---
+++ app/src/main/java/android/ninia/PlotRouteActivity.java
@@ -0,0 +1,14 @@
+package android.ninia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlotRouteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plot_route);
+    }
+}
--- app/src/main/java/android/ninia/RunningActivity.java
+++ app/src/main/java/android/ninia/RunningActivity.java
@@ -1,16 +1,14 @@
 package android.ninia;
 
-//TESTING GITHUB
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class RunningActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_running);
     }
 }
